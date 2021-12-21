73a8d5c6917a95d397708c14ed284807b4237539
Anthony Audrain <anthonyaud78@gmail.com>
2021-12-21 02:51:13 +0100

    modification route post

--- backend/controllers/sauces.js
+++ backend/controllers/sauces.js
@@ -3,10 +3,7 @@ const modelSauce = require("../models/sauces");
 
 exports.sauce = (req, res) => {
     modelSauce.find()
-    .then (sauces => {
-        console.log(sauces);
-        res.status(200).json(sauces);
-    })
+    .then (sauces => res.status(200).json(sauces))
     .catch(error => res.status(400).json({error}));
 }
 exports.sauceId = (req, res) => {
@@ -14,13 +11,21 @@ exports.sauceId = (req, res) => {
 }
 
 exports.sendSauce = (req, res) => {
-    const sauce = new modelSauce({
+    const sauceObject = JSON.parse(req.body.sauce);
+    console.log(sauceObject);
+    const Sauce = new modelSauce({
+        userId: sauceObject.userId,
+        name: sauceObject.name,
+        manufacturer: sauceObject.manufacturer,
+        description: sauceObject.description,
+        mainPepper: sauceObject.mainPepper,
+        heat: sauceObject.heat,
+        imageUrl:`${req.protocol}://${req.get('host')}/images/${req.file.filename}`,
         likes: 0,
         dislikes: 0,
         usersLiked: [],
-        usersDisliked: [],
+        usersDisliked: []
     })
-    sauce.save();
-    console.log(sauce);
+    Sauce.save();
     res.status(201).json({message : "Sauce créer"});
 }
--- backend/models/sauces.js
+++ backend/models/sauces.js
@@ -1,17 +1,19 @@
 
 const mongoose = require("mongoose");
 
+
 const schemaSauces = mongoose.Schema({
-    name : { type: String},
-    manufacturer : {type: String},
-    description : {type: String},
-    mainPepper : {type: String},
-    imageUrl : {type: String},
-    heat : {type: Number},
-    likes : {type: Number},
-    dislikes : {type: Number},
-    usersLiked : {type: Array},
-    usersDisliked : {type: Array}
+    userId: {tpye : String},
+    name: { type: String, required: true},
+    manufacturer: {type: String, required: true},
+    description: {type: String, required: true},
+    mainPepper: {type: String, required: true},
+    imageUrl: {type: String, required: true},
+    heat: {type: Number, required: true},
+    likes: {type: Number},
+    dislikes: {type: Number},
+    usersLiked: {type: Array},
+    usersDisliked: {type: Array}
 })
 
 module.exports = mongoose.model("sauces", schemaSauces);
